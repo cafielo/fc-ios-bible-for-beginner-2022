a6b3995cb7cc99a994e7af9087faa33d24e76cc8
Joonwon Lee <cafielo@gmail.com>
2022-05-30 01:41:28 +0900

    starter 잘못되어 있는것 수정

--- Part3_iOS_Development_Basic/Ch6-CollectionView_DiffableDataSource/06_03-SpotifyPaywall/starter/06_03-SpotifyPaywall/SpotifyPaywall/BannerCell.swift
+++ Part3_iOS_Development_Basic/Ch6-CollectionView_DiffableDataSource/06_03-SpotifyPaywall/starter/06_03-SpotifyPaywall/SpotifyPaywall/BannerCell.swift
@@ -8,18 +8,5 @@
 import UIKit
 
 class BannerCell: UICollectionViewCell {
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var thumbnailImageView: UIImageView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.layer.cornerRadius = 16
-    }
-    
-    func configure(_ info: BannerInfo) {
-        titleLabel.text = info.title
-        descriptionLabel.text = info.description
-        thumbnailImageView.image = UIImage(named: info.imageName)
-    }
 }
--- Part3_iOS_Development_Basic/Ch6-CollectionView_DiffableDataSource/06_03-SpotifyPaywall/starter/06_03-SpotifyPaywall/SpotifyPaywall/PaywallViewController.swift
+++ Part3_iOS_Development_Basic/Ch6-CollectionView_DiffableDataSource/06_03-SpotifyPaywall/starter/06_03-SpotifyPaywall/SpotifyPaywall/PaywallViewController.swift
@@ -13,63 +13,7 @@ import UIKit
 
 class PaywallViewController: UIViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
-    @IBOutlet weak var pageControl: UIPageControl!
-    
-    let bannerInfos: [BannerInfo] = BannerInfo.list
-    let colors: [UIColor] = [.systemPurple, .systemOrange, .systemPink, .systemRed]
-    
-    enum Section {
-        case main
-    }
-    
-    typealias Item = BannerInfo
-    var datasource: UICollectionViewDiffableDataSource<Section, Item>!
-    
-    override func viewDidLoad() {
+     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // presentation: diffable datasource
-        datasource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
-            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as? BannerCell else {
-                return nil
-            }
-            cell.configure(item)
-            cell.backgroundColor = self.colors[indexPath.item]
-            return cell
-        })
-        
-        // data: snapshot
-        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
-        snapshot.appendSections([.main])
-        snapshot.appendItems(bannerInfos, toSection: .main)
-        datasource.apply(snapshot)
-        
-        // layout: compositional layout
-        collectionView.collectionViewLayout = layout()
-        collectionView.alwaysBounceVertical = false
-        
-        pageControl.numberOfPages = bannerInfos.count
-    }
-    
-    private func layout() -> UICollectionViewCompositionalLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.8), heightDimension: .absolute(200))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.orthogonalScrollingBehavior = .groupPagingCentered
-        section.interGroupSpacing = 20
-        
-        section.visibleItemsInvalidationHandler = { (item, offset, env) in
-            let index = Int((offset.x / env.container.contentSize.width).rounded(.up))
-            print(">>> \(index)")
-            self.pageControl.currentPage = index
-        }
-        
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
     }
 }
