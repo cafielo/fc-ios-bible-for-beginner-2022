7d3183b661d8a0ef5ff02bc91cc05666f6dc99fc
Joonwon Lee <cafielo@gmail.com>
2022-06-01 10:47:28 +0900

    Part7 add 01 list starter

---
+++ Part7_SwiftUI/Ch5_SwiftUI-List_and_Grid/01-StockRank-SwiftUI/starter/StockRank-SwiftUI/StockRank-SwiftUI/StockRankView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StockRank-SwiftUI
+//
+//  Created by joonwon lee on 2022/05/21.
+//
+
+import SwiftUI
+
+struct StockRankView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankView()
+    }
+}
---
+++ Part7_SwiftUI/Ch5_SwiftUI-List_and_Grid/01-StockRank-SwiftUI/starter/StockRank-SwiftUI/StockRank-SwiftUI/StockRank_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockRank_SwiftUIApp.swift
+//  StockRank-SwiftUI
+//
+//  Created by joonwon lee on 2022/05/21.
+//
+
+import SwiftUI
+
+@main
+struct StockRank_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StockRankView()
+        }
+    }
+}
