cf669bccdeeb88878b0b21cc9c8903e08990c63b
Joonwon Lee <cafielo@gmail.com>
2022-06-06 17:13:50 +0900

    Part7 ch6 starter 업데이트

---
+++ Part7_SwiftUI/Ch6_SwiftUI-Navigation-이해하기/starter/StockRank-SwiftUI/StockRank-SwiftUI/StockRankView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  StockRank-SwiftUI
+//
+//  Created by joonwon lee on 2022/05/21.
+//
+
+import SwiftUI
+
+struct StockRankView: View {
+    
+    @State var list = StockModel.list
+    
+    var body: some View {
+        
+        List(list) { item in
+            StockRankRow(stock: item)
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowSeparator(.hidden)
+                .frame(height: 80)
+        }
+        .listStyle(.plain)
+        .background(.black)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankView()
+    }
+}
