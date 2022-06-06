00d4b6e3dc3475d150161ac8f1f5d2d03a9dfaed
Joonwon Lee <cafielo@gmail.com>
2022-06-06 17:54:20 +0900

    Part7 ch6 navigation view 완료

---
+++ Part7_SwiftUI/Ch6_SwiftUI-Navigation-이해하기/final/StockRank-SwiftUI/StockRank-SwiftUI/StockRankView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  StockRank-SwiftUI
+//
+//  Created by joonwon lee on 2022/05/21.
+//
+
+import SwiftUI
+
+struct StockRankView: View {
+    
+    @State var list = StockModel.list
+    
+    var body: some View {
+        NavigationView {
+            List($list) { $item in
+                ZStack {
+                    NavigationLink {
+                        StockDetailView(stock: $item)
+                    } label: {
+                        EmptyView()
+                    }
+                    StockRankRow(stock: $item)
+                }
+                .listRowInsets(EdgeInsets())
+                .frame(height: 80)
+            }
+            .listStyle(.plain)
+            .navigationTitle("Stock Rank")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        StockRankView()
+            .preferredColorScheme(.dark)
+    }
+}
