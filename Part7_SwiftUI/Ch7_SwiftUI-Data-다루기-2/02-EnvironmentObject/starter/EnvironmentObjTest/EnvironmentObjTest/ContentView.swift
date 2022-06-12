9aa014c5e03194db0dc30eaa1878e03bd8a70508
Joonwon Lee <cafielo@gmail.com>
2022-06-12 22:38:36 +0900

    Part7 Ch7 02 environment object 추가

---
+++ Part7_SwiftUI/Ch7_SwiftUI-Data-다루기-2/02-EnvironmentObject/starter/EnvironmentObjTest/EnvironmentObjTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EnvironmentObjTest
+//
+//  Created by joonwon lee on 2022/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Part7_SwiftUI/Ch7_SwiftUI-Data-다루기-2/02-EnvironmentObject/starter/EnvironmentObjTest/EnvironmentObjTest/EnvironmentObjTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvironmentObjTestApp.swift
+//  EnvironmentObjTest
+//
+//  Created by joonwon lee on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct EnvironmentObjTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
